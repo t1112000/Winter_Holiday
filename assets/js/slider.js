7ccfee9a394c08f7b5c1a7e84361a209d705ec02
Tuyen <9a5.tuyen@gmail.com>
2021-07-19 00:46:23 +0700

    update slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,4 @@
-const slider = document.querySelector(".container__banner");
 const slides = document.querySelectorAll(".container__banner-item");
-const slideIcons = document.querySelectorAll(".reviews-btn");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const numberOfSlider = slides.length;
@@ -10,9 +8,6 @@ const Slider = () => {
     slides.forEach((slide) => {
       slide.classList.remove("active");
     });
-    slideIcons.forEach((dot) => {
-      dot.classList.remove("active");
-    });
 
     slideNumber++;
 
@@ -21,15 +16,11 @@ const Slider = () => {
     }
 
     slides[slideNumber].classList.add("active");
-    // slideIcons[slideNumber].classList.add("active");
   });
 
   prevBtn.addEventListener("click", () => {
     slides.forEach((slide) => {
       slide.classList.remove("active");
-    });
-    slideIcons.forEach((dot) => {
-      dot.classList.remove("active");
     });
 
     slideNumber--;
@@ -39,16 +30,12 @@ const Slider = () => {
     }
 
     slides[slideNumber].classList.add("active");
-    // slideIcons[slideNumber].classList.add("active");
   });
-  var playSlider;
+
   const autoSlider = () => {
-    playSlider = setInterval(() => {
+    setInterval(() => {
       slides.forEach((slide) => {
         slide.classList.remove("active");
-      });
-      slideIcons.forEach((dot) => {
-        dot.classList.remove("active");
       });
 
       slideNumber++;
@@ -58,34 +45,9 @@ const Slider = () => {
       }
 
       slides[slideNumber].classList.add("active");
-      //   slideIcons[slideNumber].classList.add("active");
     }, 8000);
   };
-
   autoSlider();
-
-  //  stop the image slider autoplay on mouseover
-  slider.addEventListener("mouseover", () => {
-    clearInterval(playSlider);
-  });
-
-  slider.addEventListener("mouseout", () => {
-    autoSlider();
-  });
-
-  slideIcons.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-      slides.forEach((slide) => {
-        slide.classList.remove("active");
-      });
-      slideIcons.forEach((dot) => {
-        dot.classList.remove("active");
-      });
-
-      slides[index].style.display = "block";
-      //   slideIcons[index].classList.add("active");
-    });
-  });
 };
 
 export default Slider;
